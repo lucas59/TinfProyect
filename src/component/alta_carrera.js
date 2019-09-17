1f5892337f765dae2b286e2f22b86130e07a7477
isma4102 <santoscastelvi2014@gmail.com>
2019-09-17 19:14:47 -0300

    rutas

---
+++ src/component/alta_carrera.js
@@ -0,0 +1,19 @@
+import React, { Component, View, Text } from 'react';
+import Cabecera from './navigation'
+
+class alta_carrera extends Component {
+
+    constructor() {
+        super();
+
+    }
+
+
+    render() {
+        return (
+            <h1 >Alta carrera</h1>
+        );
+    }
+}
+
+export default alta_carrera;
