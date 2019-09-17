65443da60082eacc8a6759ba4acf3b3d539b0c23
MartinVideverrigain <40012749+MartinVideverrigain@users.noreply.github.com>
2019-09-17 19:20:28 -0300

    b

---
+++ src/component/materia.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class materia extends component {
+    constructor() {
+        super();
+     
+
+    }
+
+    render() {
+        return (
+            <h1> una materia </h1>
+        );
+    }
+}
+
+export default materia;
