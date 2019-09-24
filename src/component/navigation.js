3a4ed58ec2d70548bd45aa8ed4d7bbaa392f90f1
Lucas Ciceri <lucasciceri59@gmail.com>
2019-09-24 19:06:27 -0300

    Update navigation.js

--- src/component/navigation.js
+++ src/component/navigation.js
@@ -41,29 +41,25 @@ const Cabecera = ({ match }) => (
         <Route path="/ingresar" component={Login} />
 
             <Router>
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Inicio<span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item">
-                                <Link to={`/altaCarrera`}>Alta carrera</Link>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Pricing</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
+            <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+            <NavLink class='navbar-brand' style={{ color: 'white' }} to="/">Inicio</NavLink>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <NavLink className={styles.links} to="/altaCarrera">Alta carrera</NavLink>
+                    </li>
+                    <li class="nav-item">
+                        <NavLink className={styles.links} to="/modificarCarrera">Modificar carrera</NavLink>
+                    </li>
+                    <li class="nav-item">
+                        <NavLink className={styles.links} to="/eliminarCarrera">Eliminar carrera</NavLink>
+                    </li>
+                </ul>
+            </div>
+        </nav>
                     <Switch>
                         <Route exact path="/" component={Body} />
                         <Route path="/altaCarrera" component={altaCarrera} />
