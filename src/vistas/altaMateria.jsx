78a6e0016e8209537db626628a7f3521cb557238
MartinVideverrigain <40012749+MartinVideverrigain@users.noreply.github.com>
2019-09-26 11:01:29 -0300

    n

--- src/vistas/altaMateria.jsx
+++ src/vistas/altaMateria.jsx
@@ -3,14 +3,31 @@ import React from 'react';
 const template = () => (
     <div>
         <h1>Ingresar nueva materia</h1>
-        <input type="text" placeholder="Nombre..." name="nombre" />
-        <input type="text" placeholder="Creditos..." name="creditos" />
-        <input type="text" placeholder="Semestre..." name="semestre" />
-        <input type="text" placeholder="Objetivo de la asignatura..." name="objetivo" />
-        <textarea rows="30" cols="50" placeholder="Temario..."></textarea>
-        <input type="text" placeholder="Metodología de enseñanza..." name="metodologia" />
-        <input type="text" placeholder="Bibliografía..." name="bibliografia" />
-        <input type="text" placeholder="Formas de evaluación..." name="evaluacion" />
+        <label htmlFor="Nombre">Nombre:</label>
+        <input type="text" name="nombre" id="Nombre" />
+        <label htmlFor="Creditos">Creditos:</label>
+        <input type="text" name="creditos" id="Creditos" />
+        <label htmlFor="Semestre">Semestre:</label>
+        <select name="semestre" id="Semestre">
+            <option value="1"></option>
+            <option value="2"></option>
+            <option value="3"></option>
+            <option value="4"></option>
+            <option value="5"></option>
+            <option value="6"></option>
+        </select>
+        <label htmlFor="Objetivo">Objetivo:</label>
+        <input type="text" name="objetivo" id="Objetivo" />
+        <label htmlFor="Temario">Temario:</label>
+        <textarea rows="30" cols="50" name="temario" id="Temario"></textarea>
+        <label htmlFor="Metodologia">Metodología:</label>
+        <input type="text" name="metodologia" id="Metodologia" />
+        <label htmlFor="Bibliografia">Bibliografía:</label>
+        <input type="text" name="bibliografia" id="Bibliografia" />
+        <label htmlFor="Evaluacion">Formas de evaluación:</label>
+        <input type="text" name="evaluacion" id="Evolucion" />
+        <input type="button" name="aceptar" value="Aceptar" />
     </div>
-
 );
+
+export default template;
