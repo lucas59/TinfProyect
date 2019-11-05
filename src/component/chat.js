8ea1a67a64a596160250ddafd16aabe7b6eeddf2
isma4102 <santoscastelvi2014@gmail.com>
2019-11-04 21:03:23 -0300

    chat

--- src/component/chat.js
+++ src/component/chat.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
-import openSocket from 'socket.io-client';
-const socket = openSocket('http://localhost:3050');
+import socketIOClient from "socket.io-client";
 class chat extends Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
         this.state = {
-        }
-    };
-
-
+            response: 0,
+            endpoint: "http://localhost:3050"
+        };
+    }
+    componentDidMount = () => {
+        const {endpoint} = this.state;
+        //Very simply connect to the socket
+        const socket = socketIOClient(endpoint);
+        //Listen for data on the "outgoing data" namespace and supply a callback for what to do when we get one. In this case, we set a state variable
+        socket.on("outgoing data", data =>console.log( data.num));
+    }
     render() {
-        socket.emit('click', 1000);
         console.log("prueba");
         return (
             <>
-               
+
             </>
         )
 
