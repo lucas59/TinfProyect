3b49687e7c087b02862073810eb808cb2ee210de
isma4102 <santoscastelvi2014@gmail.com>
2019-11-05 10:45:04 -0300

    chat

--- src/component/chat.js
+++ src/component/chat.js
@@ -9,13 +9,15 @@ class chat extends Component {
         };
     }
     componentDidMount = () => {
+      
+        //Listen for data on the "outgoing data" namespace and supply a callback for what to do when we get one. In this case, we set a state variable
+        
+    }
+    render() {
         const {endpoint} = this.state;
         //Very simply connect to the socket
         const socket = socketIOClient(endpoint);
-        //Listen for data on the "outgoing data" namespace and supply a callback for what to do when we get one. In this case, we set a state variable
-        socket.on("outgoing data", data =>console.log( data.num));
-    }
-    render() {
+        socket.emit("click", sessionStorage.getItem("session"));
         console.log("prueba");
         return (
             <>
